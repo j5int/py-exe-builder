d43dc8458e3ac12863e44b72bbe12f81c9c20f00
Matt Hampton <matth@j5int.com>
2014-11-03 15:08:23 +0200

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,28 @@
+
+__author__ = 'matth'
+
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='py-exe-builder',
+    version='0.1',
+    packages = find_packages(),
+    license='Apache License, Version 2.0',
+    description='Uses py2exe to create small exe stubs that leverage a full python installation, rather than packing the required pyc files in to the executable.',
+    long_description=open('README.md').read(),
+    url='http://www.j5int.com/',
+    author='j5 International',
+    author_email='support@j5int.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: Apache Software License',
+        'Environment :: Win32 (MS Windows)',
+        'Intended Audience :: Developers',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2 :: Only',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    install_requires = ["py2exe"],
+)
